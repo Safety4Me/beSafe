1297ff20ff07435a40a5d8374ac102b3700f3f7c
NeyDavid <dvelosofilho@gmail.com>
2023-01-16 10:07:45 -0300

    Fix Besafe Import

--- lib/besafe.dart
+++ lib/besafe.dart
@@ -1,4 +1,14 @@
 export 'app/core/widgets/app_bar_widget.dart';
+export 'app/core/widgets/app_bar_title_widget.dart';
 export 'app/core/widgets/arrow_widget.dart';
 export 'app/core/widgets/body_text_card_widget.dart';
 export 'app/core/widgets/button_row_card_widget.dart';
+export 'app/core/widgets/circular_progress_indicator_widget.dart';
+export 'app/core/widgets/custom_alert_widget.dart';
+export 'app/core/widgets/custom_button_no_shadow.dart';
+export 'app/core/widgets/custom_button.dart';
+export 'app/core/widgets/custom_logos.dart';
+export 'app/core/widgets/custom_slider_thumb_rect_widget.dart';
+export 'app/core/widgets/pacific_blue_button_widget.dart';
+export 'app/core/widgets/slider_evaluation_widget.dart';
+export 'app/core/widgets/slider_widget.dart';
