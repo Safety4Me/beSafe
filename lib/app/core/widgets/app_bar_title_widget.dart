f7123f3bae1e282c72a4d1b83d03ee39b7b9dbd2
NeyDavid <dvelosofilho@gmail.com>
2023-02-09 23:35:46 -0300

    Chore [AppBar] IsLandscape

--- lib/app/core/widgets/app_bar_title_widget.dart
+++ lib/app/core/widgets/app_bar_title_widget.dart
@@ -6,16 +6,20 @@ class AppBarTitleWidget extends StatelessWidget {
   final Uint8List? customLogo;
   final double height;
   final String logoSafety4me;
+  final bool isLandscape;
   const AppBarTitleWidget({
     Key? key,
     required this.customLogo,
     required this.height,
     required this.logoSafety4me,
+    required this.isLandscape,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: customLogo != null ? Alignment.center : Alignment.centerLeft,
+      alignment: customLogo != null || isLandscape
+          ? Alignment.center
+          : Alignment.centerLeft,
       child: Padding(
         padding: const EdgeInsets.only(top: 2.0, bottom: 2.0),
         child: customLogo != null
